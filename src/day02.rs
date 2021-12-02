215c6097ef9fc02294a7a6051deeb3f2c1862691
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2021-12-02 14:42:29 -0500

    day 2, part 2

--- src/day02.rs
+++ src/day02.rs
@@ -25,11 +25,13 @@ fn get_position_change(command: &str, units: i32) -> Position {
     }
 }
 
-pub fn run_part1() -> i32 {
+fn get_file_content() -> String {
     let filename = "day02.txt";
-    let content =
-        fs::read_to_string(filename).expect(&format!("Could not read file from {}", filename));
+    fs::read_to_string(filename).expect(&format!("Could not read file from {}", filename))
+}
 
+pub fn run_part1() -> i32 {
+    let content = get_file_content();
     let Position { x, y } = content
         .lines()
         .fold(Position { x: 0, y: 0 }, |position, current| {
@@ -46,12 +48,57 @@ pub fn run_part1() -> i32 {
     x * y
 }
 
+struct ModifiedPosition {
+    x: i32,
+    y: i32,
+    aim: i32
+}
+
+impl Add for ModifiedPosition {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        ModifiedPosition { x: self.x + rhs.x, y: self.y + rhs.y, aim: self.aim + rhs.aim }
+    }
+}
+
+fn get_modified_position_change(command: &str, units: i32, current_aim: i32) -> ModifiedPosition {
+    match command {
+        "forward" => ModifiedPosition { x: units, y: current_aim * units, aim: 0 },
+        "up" => ModifiedPosition { x: 0, y: 0, aim: -units },
+        "down" => ModifiedPosition { x: 0, y: 0, aim: units },
+        _ => panic!("received unknown command"),
+    }
+}
+
+pub fn run_part2() -> i32 {
+    let content = get_file_content();
+    let ModifiedPosition { x, y, .. } = content.lines().fold(ModifiedPosition { x: 0, y: 0, aim: 0 }, |position, current| {
+        let mut line = current.split_whitespace();
+        let command = line.next().expect("cannot parse command from line");
+        let units = line
+            .next()
+            .expect("cannot parse unit from line")
+            .parse::<i32>()
+            .expect("cannot parse unit into i32");
+        let current_aim = position.aim;
+        position + get_modified_position_change(command, units, current_aim)
+    });
+
+    x * y
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::day02::run_part1;
+    use crate::day02::{run_part1, run_part2};
 
     #[test]
     fn part1_correct() {
-        assert_eq!(2102357, run_part1())
+        assert_eq!(2102357, run_part1());
+    }
+
+    #[test]
+    fn part2_correct() {
+        assert_eq!(2101031224, run_part2());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod day01;
 mod day02;
 
+// advent of code 2021
 fn main() {
     day01::run_part1();
     day01::run_part2();
     day02::run_part1();
+    day02::run_part2();
 }
