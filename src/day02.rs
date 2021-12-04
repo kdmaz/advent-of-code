e0909bb8086f157c6f32eb34a014cb081f02962b
kyle <kyle.mazeika@gmail.com>
2021-12-04 15:12:13 -0500

    allows dead code

--- src/day02.rs
+++ src/day02.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use crate::util;
 use std::ops::Add;
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,2 @@
-mod day01;
-mod day02;
-mod day03;
-mod util;
-
 // advent of code 2021
-fn main() {
-    day01::run_part1();
-    day01::run_part2();
-    day02::run_part1();
-    day02::run_part2();
-    day03::run_part1();
-    day03::run_part2();
-}
+fn main() {}
