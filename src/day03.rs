4da96e837ae2869716f6b2c9c4f265a5d70f21e4
kyle <kyle.mazeika@gmail.com>
2021-12-04 15:07:32 -0500

    refactor day3

--- src/day03.rs
+++ src/day03.rs
@@ -1,51 +1,4 @@
-use std::collections::HashMap;
-
 use crate::util;
-
-pub fn run_part1() -> i32 {
-    let mut ones_zeros_count_map = HashMap::new();
-
-    let content = util::get_file_content("day03.txt");
-
-    content.lines().for_each(|line| {
-        line.chars().enumerate().for_each(|(i, c)| {
-            let contains_index = ones_zeros_count_map.contains_key(&i);
-
-            if !contains_index {
-                ones_zeros_count_map.insert(i, (0, 0));
-            }
-
-            let ones_zeros_count = ones_zeros_count_map.get(&i).unwrap();
-            let ones_count = ones_zeros_count.0;
-            let zeros_count = ones_zeros_count.1;
-
-            match c {
-                '0' => ones_zeros_count_map.insert(i, (ones_count, zeros_count + 1)),
-                '1' => ones_zeros_count_map.insert(i, (ones_count + 1, zeros_count)),
-                c @ _ => panic!("recieved a char other than a 1 or a 0: ({})", c),
-            };
-        })
-    });
-
-    let mut gamma = String::new();
-    let mut epsilon = String::new();
-
-    for i in 0..12 {
-        let (ones_count, zeros_count) = ones_zeros_count_map.get(&i).unwrap();
-
-        if ones_count > zeros_count {
-            gamma.push_str("1");
-            epsilon.push_str("0");
-        } else {
-            gamma.push_str("0");
-            epsilon.push_str("1");
-        }
-    }
-
-    let gamma = i32::from_str_radix(&gamma, 2).unwrap();
-    let epsilon = i32::from_str_radix(&epsilon, 2).unwrap();
-    gamma * epsilon
-}
 
 struct BitCount {
     zero_count: i32,
@@ -94,6 +47,23 @@ fn get_least_common_bit_by_index(i: usize, lines: &Vec<&str>) -> char {
     }
 }
 
+pub fn run_part1() -> i32 {
+    let content = util::get_file_content("day03.txt");
+    let lines = content.lines().collect::<Vec<&str>>();
+    let first_line = lines.first().expect("error getting first line of content");
+    let mut gamma = String::new();
+    let mut epsilon = String::new();
+
+    first_line.chars().enumerate().for_each(|(i, _)| {
+        gamma.push_str(&get_most_common_bit_by_index(i, &lines).to_string());
+        epsilon.push_str(&get_least_common_bit_by_index(i, &lines).to_string());
+    });
+
+    let gamma = i32::from_str_radix(&gamma, 2).unwrap();
+    let epsilon = i32::from_str_radix(&epsilon, 2).unwrap();
+    gamma * epsilon
+}
+
 fn filter_lines_by_bits(
     mut lines: Vec<&str>,
     operation: fn(i: usize, lines: &Vec<&str>) -> char,
@@ -123,20 +93,18 @@ fn filter_lines_by_bits(
         panic!("no items remain in lines")
     }
 
-    let bits = *lines
+    let bits = lines
         .first()
         .expect("could not get the first item from lines");
 
-    i32::from_str_radix(&bits, 2).expect(&format!("could not convert bits ({}) to integer", bits))
+    i32::from_str_radix(bits, 2).expect(&format!("could not convert bits ({}) to integer", bits))
 }
 
 pub fn run_part2() -> i32 {
     let content = util::get_file_content("day03.txt");
     let lines = content.lines().collect::<Vec<&str>>();
-    let lines_copy = lines.to_vec();
-
-    let oxygen_generator = filter_lines_by_bits(lines, get_most_common_bit_by_index);
-    let co2_scrubber = filter_lines_by_bits(lines_copy, get_least_common_bit_by_index);
+    let oxygen_generator = filter_lines_by_bits(lines.to_vec(), get_most_common_bit_by_index);
+    let co2_scrubber = filter_lines_by_bits(lines, get_least_common_bit_by_index);
 
     oxygen_generator * co2_scrubber
 }
