3ab16f0ad90c61d045ac8d09ba7e26faa1acac89
kyle <kyle.mazeika@gmail.com>
2021-12-05 12:39:58 -0500

    adds template

---
+++ src/day06.rs
@@ -0,0 +1,39 @@
+// https://adventofcode.com/2021/day/6
+
+pub fn run_part1(path: &str) -> i32 {
+    0
+}
+
+pub fn run_part2(path: &str) -> i32 {
+    0
+}
+
+fn main() {}
+
+mod tests {
+    use crate::{run_part1, run_part2};
+
+    #[ignore]
+    #[test]
+    fn part1_example() {
+        assert_eq!(-1, run_part1("day06_example.txt"))
+    }
+
+    #[ignore]
+    #[test]
+    fn part2_example() {
+        assert_eq!(-1, run_part2("day06_example.txt"))
+    }
+
+    #[ignore]
+    #[test]
+    fn part1() {
+        assert_eq!(-1, run_part1("day06.txt"))
+    }
+
+    #[ignore]
+    #[test]
+    fn part2() {
+        assert_eq!(-1, run_part2("day06.txt"))
+    }
+}
