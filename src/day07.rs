ca8307fc2a217c322988aea7eeebb45ad20cf86a
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2021-12-07 08:11:44 -0500

    day 7, part 2

--- src/day07.rs
+++ src/day07.rs
@@ -1,10 +1,8 @@
-use std::collections::HashMap;
-
 // https://adventofcode.com/2021/day/7
 
-pub fn run_part1(path: &str) -> i32 {
-    let content = util::get_file_content(path);
-    let nums = content.split(",").map(|num| num.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+use std::collections::HashMap;
+
+fn get_max_and_min(nums: &Vec<i32>) -> (isize, isize) {
     let (max, min) = nums.iter().fold((None, None), |(max, min), current_position| {
         if max == None || min == None {
             return (Some(current_position), Some(current_position));
@@ -21,7 +19,23 @@ pub fn run_part1(path: &str) -> i32 {
         }
     });
 
-    let (max, min) = (*max.unwrap() as isize, *min.unwrap() as isize);
+    (*max.unwrap() as isize, *min.unwrap() as isize)
+}
+
+fn get_lowest_fuel_count(fuel_cost_by_position: &HashMap<isize, i32>) -> i32 {
+    *fuel_cost_by_position.iter().fold(None, |cheapest_fuel_cost, (_, fuel_cost)| {
+        if cheapest_fuel_cost == None || fuel_cost < cheapest_fuel_cost.unwrap() {
+            Some(fuel_cost)
+        } else {
+            cheapest_fuel_cost
+        }
+    }).unwrap()
+}
+
+pub fn run_part1(path: &str) -> i32 {
+    let content = util::get_file_content(path);
+    let nums = content.split(",").map(|num| num.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let (max, min) = get_max_and_min(&nums);
 
     let mut fuel_cost_by_position = HashMap::new();
 
@@ -33,17 +47,26 @@ pub fn run_part1(path: &str) -> i32 {
         fuel_cost_by_position.insert(position, fuel_cost);
     }
 
-    *fuel_cost_by_position.iter().fold(None, |cheapest_fuel_cost, (_, fuel_cost)| {
-        if cheapest_fuel_cost == None || fuel_cost < cheapest_fuel_cost.unwrap() {
-            Some(fuel_cost)
-        } else {
-            cheapest_fuel_cost
-        }
-    }).unwrap()
+    get_lowest_fuel_count(&fuel_cost_by_position)
 }
 
 pub fn run_part2(path: &str) -> i32 {
-    0
+    let content = util::get_file_content(path);
+    let nums = content.split(",").map(|num| num.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let (max, min) = get_max_and_min(&nums);
+
+    let mut fuel_cost_by_position = HashMap::new();
+
+    for position in min..=max {
+        let fuel_cost = nums.iter().fold(0, |fuel_cost, current_position| {
+            let difference = (current_position - position as i32).abs();
+            fuel_cost + (0..=difference).sum::<i32>()
+        });
+
+        fuel_cost_by_position.insert(position, fuel_cost);
+    }
+
+    get_lowest_fuel_count(&fuel_cost_by_position)
 }
 
 fn main() {}
@@ -57,10 +80,9 @@ mod tests {
         assert_eq!(37, run_part1("day07_example.txt"))
     }
 
-    #[ignore]
     #[test]
     fn part2_example() {
-        assert_eq!(-1, run_part2("day07_example.txt"))
+        assert_eq!(168, run_part2("day07_example.txt"))
     }
 
     #[test]
@@ -68,9 +90,8 @@ mod tests {
         assert_eq!(347011, run_part1("day07.txt"))
     }
 
-    #[ignore]
     #[test]
     fn part2() {
-        assert_eq!(-1, run_part2("day07.txt"))
+        assert_eq!(98363777, run_part2("day07.txt"))
     }
 }
