dc9897e7ce83c356a7d36d50f82f3c79708895e2
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2021-12-08 17:10:02 -0500

    wip

--- src/day08.rs
+++ src/day08.rs
@@ -1,22 +1,23 @@
-use std::collections::HashMap;
-
 // https://adventofcode.com/2021/day/8
 
-pub fn run_part1(path: &str) -> i32 {
+use std::collections::{HashMap, HashSet};
+
+
+fn get_formatted_content(path: &str) -> String {
 	let content = util::get_file_content(path);
 
-	// 10 | 4
-	let x;
-
 	if cfg!(windows) {
-		x = content.replace("|\r\n", "| ");
+		content.replace("|\r\n", "| ")
 	} else {
-		x = content.replace("|\n", "| ");
-	}
+		content.replace("|\n", "| ")
+	}	
+}
 
+pub fn run_part1(path: &str) -> i32 {
+	let content = get_formatted_content(path);
 	let mut segment_len_map = HashMap::new();
-
-	x.lines().for_each(|line| {
+	
+	content.lines().for_each(|line| {
 		let mut line = line.split("|");
 		line.next();
 		line.next().unwrap().trim().split_whitespace().for_each(|segment| {
@@ -34,7 +35,44 @@ pub fn run_part1(path: &str) -> i32 {
 }
 
 pub fn run_part2(path: &str) -> i32 {
-	0
+	let content = get_formatted_content(path);
+
+	content.lines().fold(0, |total_value_count, line| {
+		let mut line = line.split("|");
+
+		// let char_by_position = HashMap::new();
+
+		line.next().unwrap().trim().split_whitespace();
+
+		let value = line.next().unwrap().trim().split_whitespace().map(|segment| {
+			match segment.len() {
+				2 => return "1",
+				4 => return "4",
+				3 => return "3",
+				7 => return "8",
+				_ => {}
+			}
+
+			let mut sorted_segment = segment.chars().collect::<Vec<char>>();
+			sorted_segment.sort();
+			let sorted_segment = sorted_segment.iter().collect::<String>();
+
+			match sorted_segment.as_str() {
+				"ab" => "1",
+				"acdfg" => "2",
+				"abcdf" => "3",
+				"abef" => "4",
+				"bcdef" => "5",
+				"bcdefg" => "6",
+				"abd" => "7",
+				"abcdefg" => "8",
+				"abcdef" => "9",
+				"abcdeg" => "0",
+				c @ _ => panic!("could not find digit that matches ({})", c)
+			}
+		}).collect::<String>().parse::<i32>().unwrap();
+		total_value_count + value
+	})
 }
 
 fn main() {}
@@ -48,10 +86,9 @@ mod tests {
 			assert_eq!(26, run_part1("day08_example.txt"))
 	}
 
-	#[ignore]
 	#[test]
 	fn part2_example() {
-			assert_eq!(-1, run_part2("day08_example.txt"))
+			assert_eq!(61229, run_part2("day08_example.txt"))
 	}
 
 	#[test]
