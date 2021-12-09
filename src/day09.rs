f83e803e9c97ccd05bd5fe20f10c550fd55e9c5c
kyle <kyle.mazeika@gmail.com>
2021-12-08 22:34:53 -0500

    day 9 boilerplate

---
+++ src/day09.rs
@@ -0,0 +1,40 @@
+// https://adventofcode.com/2021/day/9
+
+pub fn run_part1(path: &str) -> i32 {
+    0
+}
+
+pub fn run_part2(path: &str) -> i32 {
+    0
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use crate::{run_part1, run_part2};
+
+    #[ignore]
+    #[test]
+    fn part1_example() {
+        assert_eq!(-1, run_part1("day09_example.txt"))
+    }
+
+    #[ignore]
+    #[test]
+    fn part2_example() {
+        assert_eq!(-1, run_part2("day09_example.txt"))
+    }
+
+    #[ignore]
+    #[test]
+    fn part1() {
+        assert_eq!(-1, run_part1("day09.txt"))
+    }
+
+    #[ignore]
+    #[test]
+    fn part2() {
+        assert_eq!(-1, run_part2("day09.txt"))
+    }
+}
