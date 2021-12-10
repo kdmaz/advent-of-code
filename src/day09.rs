85bc8a74d22666171a053640abab3714fb0bab97
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2021-12-10 09:25:15 -0500

    recursion wip

--- src/day09.rs
+++ src/day09.rs
@@ -1,5 +1,7 @@
 // https://adventofcode.com/2021/day/9
 
+use std::{hash::Hash, collections::HashSet};
+
 pub fn run_part1(path: &str) -> i32 {
     let content = util::get_file_content(path);
     let nums_by_line = content
@@ -60,16 +62,90 @@ pub fn run_part1(path: &str) -> i32 {
         .fold(0, |count, point| count + point + 1)
 }
 
-#[derive(PartialEq, Eq, Debug, Clone)]
+#[derive(PartialEq, Eq, Debug, Clone, Hash, Copy)]
 struct Point {
     val: i32,
     x: i32,
     y: i32,
 }
 
+fn recursively_add_to_basin(basin: &mut HashSet<Point>, point: Point, row: &Vec<i32>, table: &Vec<Vec<i32>>) {
+    if basin.contains(&point) {
+        return;
+    }
+
+    basin.insert(point);
+
+    let Point { x, y, ..} = point;
+
+    // left
+    if (x - 1) >= 0 {
+        let left = row.get(x as usize - 1);
+        if left.is_some() {
+            recursively_add_to_basin(basin, Point {
+                x: x - 1,
+                y,
+                val: *left.unwrap(),
+            }, row, table);
+        };
+    };
+
+    // right
+    if (x + 1) >= 0 {
+        let right = row.get(x as usize + 1);
+        if right.is_some() {
+            recursively_add_to_basin(basin, Point {
+                x: x + 1,
+                y,
+                val: *right.unwrap(),
+            }, row, table);
+        };
+    };
+
+    // up
+    if (y - 1) >= 0 {
+        let up_row = table.get(y as usize - 1);
+        if up_row.is_some() {
+            let up = up_row.unwrap().get(x as usize);
+            if up.is_some() {
+                recursively_add_to_basin(
+                    basin,
+                    Point {
+                        x,
+                        y: y - 1,
+                        val: *up.unwrap(),
+                    },
+                    row,
+                    table,
+                );
+            }
+        }
+    };
+
+    // down
+    if (y + 1) >= 0 {
+        let down_row = table.get(y as usize + 1);
+        if down_row.is_some() {
+            let down = down_row.unwrap().get(x as usize);
+            if down.is_some() {
+                recursively_add_to_basin(
+                    basin,
+                    Point {
+                        x,
+                        y: y + 1,
+                        val: *down.unwrap(),
+                    },
+                    row,
+                    table,
+                );
+            }
+        }
+    };
+}
+
 pub fn run_part2(path: &str) -> i32 {
     let content = util::get_file_content(path);
-    let nums_by_line = content
+    let table = content
         .lines()
         .map(|l| {
             l.chars()
@@ -78,109 +154,26 @@ pub fn run_part2(path: &str) -> i32 {
         })
         .collect::<Vec<Vec<i32>>>();
 
-    let mut basins: Vec<Vec<Point>> = Vec::new();
+    let mut basins: Vec<HashSet<Point>> = Vec::new();
 
     let mut y = 0;
-    for nums in &nums_by_line {
-        for (i, num) in nums.iter().enumerate() {
-            let left = if (i as i32 - 1) < 0 {
-                None
-            } else {
-                let val = nums.get(i - 1);
-                if val.is_some() {
-                    Some(Point {
-                        x: i as i32 - 1,
-                        y,
-                        val: *val.unwrap(),
-                    })
-                } else {
-                    None
-                }
+    for row in &table {
+        for (x, num) in row.iter().enumerate() {
+            let current_point = Point {
+                val: *num,
+                x: x as i32,
+                y
             };
 
-            let right = if (i as i32 + 1) < 0 {
-                None
-            } else {
-                let val = nums.get(i + 1);
-                if val.is_some() {
-                    Some(Point {
-                        x: i as i32 + 1,
-                        y,
-                        val: *val.unwrap(),
-                    })
-                } else {
-                    None
-                }
-            };
-
-            let up = if (y as i32 - 1) < 0 {
-                None
-            } else {
-                match nums_by_line.get(y as usize - 1) {
-                    Some(row) => {
-                        let val = row.get(i);
-                        if val.is_some() {
-                            Some(Point {
-                                x: i as i32,
-                                y: y as i32 - 1,
-                                val: *val.unwrap(),
-                            })
-                        } else {
-                            None
-                        }
-                    }
-                    None => None,
-                }
-            };
-
-            let down = if (y as i32 + 1) < 0 {
-                None
-            } else {
-                match nums_by_line.get(y as usize + 1) {
-                    Some(row) => {
-                        let val = row.get(i);
-                        if val.is_some() {
-                            Some(Point {
-                                x: i as i32,
-                                y: y as i32 + 1,
-                                val: *val.unwrap(),
-                            })
-                        } else {
-                            None
-                        }
-                    }
-                    None => None,
-                }
-            };
-
-            if num != &9 {
-                let mut basin_index = None;
-                let mut index: usize = 0;
-
-                'basin: for basin in &basins {
-                    for point in basin {
-                        if left.is_some() && (point == &left.unwrap()) {
-                            basin_index = Some(index);
-                            break 'basin;
-                        }
-
-                        // right
-
-                        // up
-
-                        // down
-                    }
-                }
-
-                if basin_index.is_some() {
-                    // let mut basin = basins.get(basin_index.unwrap()).unwrap();
-                    // basin.push(Point {
-                    //     val: *num,
-                    //     x: i as i32,
-                    //     y,
-                    // });
-                }
+            if *num == 9 || basins.iter().any(|basin| basin.contains(&current_point)) {
+                continue;
             }
+
+            let mut basin = HashSet::new();
+
+            recursively_add_to_basin(&mut basin, current_point, row, &table);
+
+            basins.push(basin);
         }
         y += 1;
     }
@@ -201,10 +194,9 @@ mod tests {
         assert_eq!(15, run_part1("day09_example.txt"))
     }
 
-    #[ignore]
     #[test]
     fn part2_example() {
-        assert_eq!(-1, run_part2("day09_example.txt"))
+        assert_eq!(1134, run_part2("day09_example.txt"))
     }
 
     #[test]
