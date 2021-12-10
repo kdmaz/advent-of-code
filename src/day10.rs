892f103f2c5b00d9c99c5f522336152ceff6842e
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2021-12-10 11:44:57 -0500

    day 10 boilerplate

---
+++ src/day10.rs
@@ -0,0 +1,42 @@
+// https://adventofcode.com/2021/day/10
+
+pub fn run_part1(path: &str) -> i32 {
+	let content = util::get_file_content(path);
+	0
+}
+
+pub fn run_part2(path: &str) -> i32 {
+	let content = util::get_file_content(path);
+	0
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+	use crate::{run_part1, run_part2};
+
+	#[ignore]
+	#[test]
+	fn part1_example() {
+			assert_eq!(-1, run_part1("day10_example.txt"))
+	}
+
+	#[ignore]
+	#[test]
+	fn part2_example() {
+			assert_eq!(-1, run_part2("day10_example.txt"))
+	}
+
+	#[ignore]
+	#[test]
+	fn part1() {
+			assert_eq!(-1, run_part1("day10.txt"))
+	}
+
+	#[ignore]
+	#[test]
+	fn part2() {
+			assert_eq!(-1, run_part2("day10.txt"))
+	}
+}
