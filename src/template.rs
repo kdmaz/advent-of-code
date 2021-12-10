84a9ad760aba7e74184819bc589b6f40cd06238e
kyle <kyle.mazeika@gmail.com>
2021-12-09 19:39:42 -0500

    adds line to get content in template

--- src/template.rs
+++ src/template.rs
@@ -1,10 +1,12 @@
 // https://adventofcode.com/2021/day/
 
 pub fn run_part1(path: &str) -> i32 {
+    let content = util::get_file_content(path);
     0
 }
 
 pub fn run_part2(path: &str) -> i32 {
+    let content = util::get_file_content(path);
     0
 }
 
